3faec0285e3a131e39915f3a05ef63ef0e892f89
Elias Naur <mail@eliasnaur.com>
2019-07-21 13:38:15 +0200

    add widgets slides

--- editor.go
+++ editor.go
@@ -11,24 +11,29 @@ import (
 	"golang.org/x/image/font/sfnt"
 )
 
-// START OMIT
 func init() { // HLinitfunc
 	go func() { // HLinitfunc
 		w := app.NewWindow(nil)
 		regular, _ := sfnt.Parse(goregular.TTF) // HLdraw
 		var cfg ui.Config                       // HLdraw
-		var faces measure.Faces                 // HLdraw
 		ops := new(ui.Ops)                      // HLdraw
+		// START INIT OMIT
+		var faces measure.Faces // HLdraw
 		editor := &text.Editor{
-			Face: faces.For(regular, ui.Sp(22)),
+			Face: faces.For(regular, ui.Sp(52)),
 		}
+		editor.SetText("Hello, Gophercon! Edit me.")
+		// END INIT OMIT
 		for e := range w.Events() {
 			if e, ok := e.(app.DrawEvent); ok {
 				cfg = &e.Config                       // HLdraw
 				cs := layout.RigidConstraints(e.Size) // HLdraw
 				ops.Reset()                           // HLdraw
 				faces.Reset(cfg)                      // HLdraw
-				editor.Layout(cfg, w.Queue(), ops, cs)
+				queue := w.Queue()
+				// START OMIT
+				editor.Layout(cfg, queue, ops, cs)
+				// END OMIT
 				w.Draw(ops) // HLdraw
 			}
 		} // HLeventloop
@@ -38,5 +43,3 @@ func init() { // HLinitfunc
 func main() { // HLmain
 	app.Main() // HLmain
 } // HLmain
-
-// END OMIT
